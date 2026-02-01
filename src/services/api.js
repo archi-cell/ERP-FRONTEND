fa31f60b245699e7360cb6981154b4736f51d7b9
archi-cell <159535338+archi-cell@users.noreply.github.com>
2026-02-01 21:02:57 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://erp-backend-al1m.onrender.com",
     headers: {
         "Content-Type": "application/json"
     }
